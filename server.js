094ce3b640a324187919343e595ccbbff57b505d
Matthew Peveler <matt.peveler@gmail.com>
2018-08-08 10:28:24 -0400

    add written text to html page

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('    Alpha   : ' + event.alpha);
         console.log('    Beta    : ' + event.beta);
         console.log('    Gamma   : ' + event.gamma);
+        document.getElementById('absolute').innerText = event.absolute;
+        document.getElementById('alpha').innerText = event.alpha;
+        document.getElementById('beta').innerText = event.beta;
+        document.getElementById('gamma').innerText = event.gamma;
     }, true);
 });
