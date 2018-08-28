74d309029a0dbdcdc3aa5436dbce0b1b0412235a
Matthew Peveler <matt.peveler@gmail.com>
2018-08-28 16:52:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Set listener for changes in device orientation...');
-    window.addEventListener('deviceorientation', (event) => {
+    window.addEventListener('deviceorientationabsolute', (event) => {
         console.log('New Orientation:');
         console.log('    Absolute: ' + event.absolute);
         console.log('    Alpha   : ' + event.alpha);
         console.log('    Beta    : ' + event.beta);
         console.log('    Gamma   : ' + event.gamma);
         document.getElementById('absolute').innerText = "" + event.absolute;
-        document.getElementById('alpha').innerText = "" + event.alpha;
-        document.getElementById('beta').innerText = "" + event.beta;
-        document.getElementById('gamma').innerText = "" + event.gamma;
+        document.getElementById('alpha').innerText = "" + Math.round(event.alpha);
+        document.getElementById('beta').innerText = "" + Math.round(event.beta);
+        document.getElementById('gamma').innerText = "" + Math.round(event.gamma);
     }, true);
 });
