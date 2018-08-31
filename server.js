b12b7718f4370d3c763f96de58d63b0ec7d1f920
Matthew Peveler <matt.peveler@gmail.com>
2018-08-31 15:46:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Set listener for changes in device orientation...');
     for (let listener of ['deviceorientation', 'deviceorientationabsolute']) {        
-        window.addEventListener('deviceorientationabsolute', (event) => {
+        window.addEventListener(listener, (event) => {
             document.getElementById(listener + '-absolute').innerText = "" + event.absolute;
             document.getElementById(listener + '-alpha').innerText = "" + Math.round(event.alpha);
             document.getElementById(listener + '-beta').innerText = "" + Math.round(event.beta);
