8e713af1a4a7ade7eececf97e27ed8f1b624cd69
Matthew Peveler <matt.peveler@gmail.com>
2019-10-14 10:28:14 -0300

    add more alerts...

--- alt.js
+++ alt.js
@@ -8,11 +8,14 @@ if (navigator.permissions) {
     navigator.permissions.query({ name: "magnetometer" }),
     navigator.permissions.query({ name: "gyroscope" })
   ]).then((results) => {
+    alert('all permissions queried...');
     if (results.every(result => result.state === 'granted')) {
+      alert('all permissions granted...');
       const relative = new RelativeOrientationSensor({frequency: 60});
       let absolute = new AbsoluteOrientationSensor({frequency: 60});
       relative.onreading = () => {
         relativeDiv.innerHTML = `<pre>Relative: ${JSON.stringify(relative.quaternion, null, 2)}</pre>`;
+        alert(relative.quaternion);
       };
       relative.onerror = (event) => {
         if (event.error.name === 'NotReadableError') {
@@ -28,6 +31,9 @@ if (navigator.permissions) {
       absolute.onerror = (event) => {
         if (event.error.name === 'NotReadableError') {
           alert('absolute sensor not available');
+        }
+        else {
+          alert(event.error.name);
         }
       }
       relative.start();
