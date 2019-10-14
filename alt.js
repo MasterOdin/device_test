40aa24705a9d886242dfd76b17249b12eae32a2d
Matthew Peveler <matt.peveler@gmail.com>
2019-10-14 10:20:58 -0300

    grrr

--- alt.js
+++ alt.js
@@ -1,18 +1,16 @@
 const absoluteDiv = document.getElementById('absolute');
 const relativeDiv = document.getElementById('relative');
 
+alert('attempting to setup sensors...');
 if (navigator.permissions) {
   Promise.all([
-    navigator.permissions.query({name: 'accelerometer'}),
-    navigator.permissions.query({name: 'magnetometer'}),
-    navigator.permissions.query({name: 'gyroscope'})
+    navigator.permissions.query({ name: "accelerometer" }),
+    navigator.permissions.query({ name: "magnetometer" }),
+    navigator.permissions.query({ name: "gyroscope" })
   ]).then((results) => {
     if (results.every(result => result.state === 'granted')) {
       const relative = new RelativeOrientationSensor({frequency: 60});
       let absolute = new AbsoluteOrientationSensor({frequency: 60});
-      relative.addEventListener('reading', () => {
-
-      });
       relative.onreading = () => {
         relativeDiv.innerHTML = `<pre>Relative: ${JSON.stringify(relative.quaternion, null, 2)}</pre>`;
       };
@@ -36,4 +34,7 @@ if (navigator.permissions) {
   }).catch((err) => {
     alert(err);
   });
+}
+else {
+  alert('Could not find navigator permissions');
 }
